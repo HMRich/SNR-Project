8c4bfd2e2bf761344a3d4c2df6b6b5ff7d6a1c16
HMRich <xxrichmanxx@gmail.com>
2020-01-15 20:54:52 -0800

    Project model setup to allow CI through Azure

---
+++ Blankmon/src/application/MyClass.java
@@ -0,0 +1,13 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MyClass extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+	}
+
+}
---
+++ Blankmon/src/tests/TestCase.java
@@ -0,0 +1,13 @@
+package tests;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCase {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
