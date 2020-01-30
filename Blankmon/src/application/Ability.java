118bc558a9076bd46c084c77ab16de4a8e4da28c
Hunter <xxrichmanxx@gmail.com>
2020-01-29 19:45:24 -0800

    Copied ability class
    @gaberies made this class but azure pipelines could compile it until a file rename or relocation.

---
+++ Blankmon/src/application/Ability.java
@@ -0,0 +1,11 @@
+package application;
+
+import application.enums.Abilities;
+
+public class Ability {
+
+	private Abilities ability;
+
+	// public abstract void activateAbility(Pokemon target);
+
+}
