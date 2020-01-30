8562c11a74706cf0271c147ed5f47ce413c9a189
Hunter <xxrichmanxx@gmail.com>
2020-01-29 19:04:59 -0800

    Copied move class
    @gaberies made this class but was having a hard time getting it to pass azure pipeline check.

---
+++ Blankmon/src/application/Move.java
@@ -0,0 +1,12 @@
+package application;
+
+import Enums.MoveIds;
+
+public abstract class Move {
+	
+	private int totalMovePoints;
+	private MoveIds moveId;
+	
+	// public abstract void attack (Pokemon source, Pokemon target);
+
+}
