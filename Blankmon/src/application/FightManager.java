727c17cba632bcad52a9b8794bb7f9627ddec04e
Gabe Ries <gries@eagles.ewu.edu>
2020-01-29 18:18:33 -0800

    Created fight manager class

--- Blankmon/src/application/FightManager.java
+++ Blankmon/src/application/FightManager.java
@@ -1,10 +1,35 @@
 package application;
 
+import java.util.ArrayList;
+
 public class FightManager 
 {
-	ArrayList<Pokemon> playerTeam
+	//private ArrayList<Pokemon> playerTeam;
+	//private ArrayList<Pokemon> enemyTeam;
 	
+	public void attackEnemy(int indexOfPlayer, int indexOfEnemy, int indexOfMove) 
+	{
+		
+	}
 	
+	public void attackPlayer(int indexOfPlayer, int indexOfEnemy, int indexOfMove) 
+	{
+		
+	}
 	
+	public void itemUse(boolean isPlayer, int indexOfTeam, Enum item) 
+	{
+		
+	}
+/*	
+	public ArrayList<Pokemon> getPlayerTeam() 
+	{
+		return playerTeam;
+	}
 	
+	public ArrayList<Pokemon> getenemyTeam() 
+	{
+		return playerTeam;
+	}
+	*/
 }
