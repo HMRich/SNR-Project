e65773e5e8d00d2c8720045aed25b3379bd46d05
Hunter <xxrichmanxx@gmail.com>
2020-01-29 16:35:30 -0800

    Initial class startup

---
+++ Blankmon/src/application/MovePool.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.util.HashMap;
+
+public class MovePool {
+	
+	private static HashMap<MoveTpye, Move> moves;
+	
+	public static getMove(MoveType move) {
+		if(moves == null) {
+			generateMoves();
+		}
+		return moves.
+	}
+	
+	public static void generateMoves() {
+		moves = HashMap<MoveType, Move>();
+		moves.
+	}
+
+}
