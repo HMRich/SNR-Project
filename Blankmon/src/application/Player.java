741222430bd380a1dae08b71823fe3db46a5de8e
Colton Somes <coltonsomes@gmail.com>
2020-02-01 10:48:33 -0800

    Added Player Class

---
+++ Blankmon/src/application/Player.java
@@ -0,0 +1,20 @@
+
+package application;
+
+import java.util.ArrayList;
+
+import Enums.*;
+
+public class Player {
+	private String name;
+	private Gender gender;
+	private ArrayList<Boolean> badges; 
+	private Backpack backpack; 
+	private Image sprite; 
+	private ArrayList <PICKNAMEmon> partyBlankmon; 
+
+	public Player() {
+		
+	}
+
+}git
