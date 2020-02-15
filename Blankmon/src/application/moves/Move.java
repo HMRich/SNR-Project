1f025fc74171ef81fac2d086ae7ec2f06eff8a8a
Gabe Ries <gries@eagles.ewu.edu>
2020-02-14 22:12:17 -0800

    Added the three new variables.
    This was case 69.... Also seems to be an error with the constructor that already existed on the branch

--- Blankmon/src/application/moves/Move.java
+++ Blankmon/src/application/moves/Move.java
@@ -2,18 +2,26 @@ package application.moves;
 
 import application.Anature;
 import application.enums.MoveIds;
+import application.enums.Type;
 
 public abstract class Move
 {
 	private int mTotalMovePoints;
 	private MoveIds mMoveId;
 	private boolean mDoesDamage;
+	private boolean mIsPhysicalAttack;
+	private double mAccuracyStat;
+	private Type mType;
 
-	public Move(int totalMovePoints, MoveIds moveId, boolean DoesDamage)
+	public Move(int totalMovePoints, MoveIds moveId, boolean DoesDamage, boolean isPhysicalAttack, double AccuracyStat, Type enumType)
 	{
 		mTotalMovePoints = totalMovePoints;
 		mMoveId = moveId;
 		mDoesDamage = DoesDamage;
+		mIsPhysicalAttack = isPhysicalAttack;
+		mAccuracyStat = AccuracyStat;
+		mType = enumType;
+				
 	}
 
 	public int getTotalMovePoints()
