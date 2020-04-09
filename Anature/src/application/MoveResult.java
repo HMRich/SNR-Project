e4b4dc69bca6d67ed08968ed1d6faa01cd8003f4
Gabe Ries <gries@eagles.ewu.edu>
2020-04-09 13:19:26 -0700

    adjusting the classes based on the new versions

--- Anature/src/application/MoveResult.java
+++ Anature/src/application/MoveResult.java
@@ -1,5 +1,6 @@
 package application;
 
+import application.abillities.AbilityResult;
 import application.enums.StatusEffects;
 
 public class MoveResult extends Result
@@ -8,8 +9,9 @@ public class MoveResult extends Result
 	private String mMpTxt;
 	private int mMoveIndex;//NOTE: a moveIndex of -1 means that the move was skipped
 	private boolean mIsPlayer;
-	
-	public MoveResult(double damageDone, String dialogueTxt, int moveIndex, String mpTxt, boolean isPlayer)
+	private AbilityResult mEnemyAbilityResult;
+	private AbilityResult mPlayerAbilityResult;
+	public MoveResult(double damageDone, String dialogueTxt, int moveIndex, String mpTxt, boolean isPlayer, AbilityResult EnemyAbilityResult, AbilityResult PlayerAbilityResult)
 	{
 		super(dialogueTxt);
 
@@ -20,6 +22,8 @@ public class MoveResult extends Result
 		mMpTxt = mpTxt;
 		mMoveIndex = moveIndex;
 		mIsPlayer = isPlayer;
+		mEnemyAbilityResult = EnemyAbilityResult;
+		mPlayerAbilityResult = PlayerAbilityResult;
 	}
 
 	public double getDamageDone()
@@ -40,6 +44,16 @@ public class MoveResult extends Result
 	public boolean isPlayer()
 	{
 		return mIsPlayer;
+	}
+	
+	public AbilityResult getEnemyAbilityResult() 
+	{
+		return mEnemyAbilityResult;
+	}
+
+	public AbilityResult getPlayerAbilityResult() 
+	{
+		return mPlayerAbilityResult;
 	}
 	
 }
