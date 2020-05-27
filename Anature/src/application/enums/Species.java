df591b91d93048845e706c8fc990cd25c11ba9c4
Colton <coltonsomes@gmail.com>
2020-05-27 15:02:50 -0700

    added anature images and database changes

--- Anature/src/application/enums/Species.java
+++ Anature/src/application/enums/Species.java
@@ -2,5 +2,6 @@ package application.enums;
 
 public enum Species
 {
-	NotSet, Null
+	NotSet, Null, Sardino, Midaqua, Smoldren, Scorta, Scaldris, Sapron, Sodren, Arganian, Transgendeero, 
+	Transgendeera, Clouder, Cloutrain, Cloudoom, Belvidia
 }
